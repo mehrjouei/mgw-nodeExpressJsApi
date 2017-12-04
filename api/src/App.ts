5a8ce8025f1458baeca06daebc13fec4accfa320
vahid mehrjooei <mehrjooei.v@outlook.com>
2017-12-04 18:03:17 +0330

    user model

--- api/src/App.ts
+++ api/src/App.ts
@@ -1,19 +1,27 @@
 import * as express from 'express'
 import * as fs from 'fs'
+import * as mongoose from 'mongoose'
+import * as session from "express-session";
+
+
+
 
 export class App {
   public express: any
-
+  public db:mongoose.Connection;
   constructor() {
     this.express = express()
+    this.conncectToDatabase();
   }
-  
-  // public loadControllers(): void {
-  //   let files = fs.readdirSync(__dirname + '/controllers');
-  //   files.forEach(fileName => {
-  //     require(__dirname + '/controllers/' + fileName);
-  //   });
-  // }
+  conncectToDatabase(){
+    mongoose.connect('mongodb://localhost:27017/test');
+    this.db = mongoose.connection;
+    this.db.on('error', console.error.bind(console, 'connection error:'));
+    this.db.once('open', function() {
+      // we're connected!
+    });
+  }
+
 }
 export default new App
 
