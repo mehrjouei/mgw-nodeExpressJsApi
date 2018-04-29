92018b8205845b08cabfdb192c425dffc11ddb42
vahid mehrjooei <mehrjooei.v@outlook.com>
2018-04-29 11:59:11 +0430

    remove clients

--- backOffice-client/src/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-   
-         API_Url : "http://localhost:3000/api",
-         server : "http://localhost:3000/api/auth/authenticate/",
-         TOKEN_ENDPOINT : "http://localhost:3000/api/auth/authenticate/",
-             
-}
-    
